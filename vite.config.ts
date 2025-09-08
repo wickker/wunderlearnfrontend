ccf69490af8d03510e4ab426ea3f65da1f89c407
Dian Chun Choy <choydianchun@gmail.com>
2025-09-08 13:43:45 +0800

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react-swc"
+import { defineConfig, loadEnv, type UserConfig } from "vite"
+
+export default defineConfig(({ mode }: UserConfig) => {
+  if (!mode) return {}
+
+  const config = loadEnv(mode, process.cwd(), "")
+
+  console.log(`🍀 Mode: ${mode}`)
+  console.log(`🍀 Env: ${config.VITE_ENVIRONMENT}`)
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      port: 3000,
+    },
+    resolve: {
+      alias: {
+        "@": "/src",
+      },
+    },
+  }
+})

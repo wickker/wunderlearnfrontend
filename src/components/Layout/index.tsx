2b8fe77d3b1c1df10294a7228dbcd8c7f75aad42
Dian Chun Choy <choydianchun@gmail.com>
2025-09-09 16:21:42 +0800

    add nav header color change on scroll

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,10 +1,32 @@
+import { useMotionValueEvent, useScroll } from 'motion/react'
+import { useState } from 'react'
 import { Outlet } from 'react-router'
 
+const interpolateColor = (start: number[], end: number[], t: number) =>
+  start.map((s, i) => Math.round(s + (end[i] - s) * t))
+
 const Layout = () => {
+  const [bg, setBg] = useState('var(--gradient-sage)')
+  const { scrollY } = useScroll()
+  useMotionValueEvent(scrollY, 'change', (latest) => {
+    const maxScroll = 200
+    const start = [245, 242, 237] // cream
+    const end = [112, 169, 131] // sage
+
+    const t = Math.min(latest / maxScroll, 1)
+    const [r, g, b] = interpolateColor(start, end, t)
+    setBg(`rgb(${r},${g},${b})`)
+  })
+
   return (
     <div className='flex min-h-[100dvh] flex-col bg-cream'>
       {/* Header */}
-      <nav className='bg-(image:--gradient-sage) sticky top-0 z-10 h-18' />
+      <nav
+        className='sticky top-0 z-10 h-18 transition-colors'
+        style={{
+          background: `${bg}`,
+        }}
+      />
 
       <div className='flex flex-col items-center overflow-y-auto'>
         <Outlet />
